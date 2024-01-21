94a00bca2ceeec5d192632d0745c0457a80619b5
lofti198 <isolar2005@gmail.com>
2024-01-21 08:44:39 +0200

    removed extra password validation for dev

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -10,13 +10,28 @@ namespace ToDoList
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var devEnv = builder.Environment.IsDevelopment();
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => {
+                options.SignIn.RequireConfirmedAccount = !devEnv;
+                if (devEnv)
+                {
+                    // Password settings
+                    options.Password.RequireDigit = false;
+                    options.Password.RequiredLength = 1; // Set to 1 or a number that suits your policy
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequiredUniqueChars = 0;
+                }
+               
+         
+                })
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
