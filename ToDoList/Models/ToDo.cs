f6728f89006496b5b1e214a2e43e16c3626e43d4
lofti198 <isolar2005@gmail.com>
2024-01-21 20:45:34 +0200

    turn description into optional

--- ToDoList/Models/ToDo.cs
+++ ToDoList/Models/ToDo.cs
@@ -6,7 +6,7 @@ namespace ToDoList.Models
     {
         public int ToDoId { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public DateTime? DueDate { get; set; }
         public bool IsCompleted { get; set; }
         public int? Priority { get; set; }
@@ -14,6 +14,7 @@ namespace ToDoList.Models
         // Navigation property for the related IdentityUser
         public string UserId { get; set; }
         public virtual IdentityUser User { get; set; }
+
     }
 
 }
